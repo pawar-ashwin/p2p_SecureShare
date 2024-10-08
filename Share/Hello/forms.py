fbcf63e6a6ebf10640b9dbb0a5b10a4b75a42417
Pawar Ashwin <pashwiny2k@gmail.com>
2024-10-08 11:27:34 -0500

    login and signup button addition with popups

---
+++ Share/Hello/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is already registered.")
+        return email
